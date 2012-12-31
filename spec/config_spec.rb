c7a00aef574a1321991f7018781011a89f51ce15
sent-hil <me@sent-hil.com>
2012-12-31 03:53:12 -0600

    moved config specs to own file

---
+++ spec/config_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Saran, 'config' do
+  subject do
+    rspec = self
+    klass = Class.new do
+      include Saran
+
+      endpoint rspec.send(:mock_api_endpoint)
+    end
+  end
+
+  it 'gets endpoint' do
+    expect(subject.endpoint).to eq(mock_api_endpoint)
+  end
+
+  it 'gets default config' do
+    expect(subject.post_type).to eq(:path)
+  end
+
+  it 'sets endpoint' do
+    subject.endpoint(mock_api_endpoint)
+    expect(subject.endpoint).to eq(mock_api_endpoint)
+  end
+end
